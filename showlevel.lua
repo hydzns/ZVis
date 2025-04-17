36975eb0f999cd82d06b25ab52d62a07eec1566c
hydzns <hydrogz7@gmail.com>
2025-04-17 16:58:45 +0700

    Update showlevel.lua

--- showlevel.lua
+++ showlevel.lua
@@ -3,9 +3,11 @@ local data = player:FindFirstChild("Data")
 local level = data and data:FindFirstChild("Level")
 local value = level and level.Value or "N/A"
 
-Rayfield:Notify({
-    Title = "Player Level",
-    Content = "Current Level: " .. tostring(value),
-    Duration = 5,
-    Image = 4483362458,
-})
+if Rayfield then
+    Rayfield:Notify({
+        Title = "Player Level",
+        Content = "Current Level: " .. tostring(value),
+        Duration = 5,
+        Image = 4483362458,
+    })
+end
