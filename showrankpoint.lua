93098d89384aec7f79e4df97bbd1fc68fb07ce74
hydzns <hydrogz7@gmail.com>
2025-04-17 16:58:56 +0700

    Update showrankpoint.lua

--- showrankpoint.lua
+++ showrankpoint.lua
@@ -3,9 +3,11 @@ local data = player:FindFirstChild("Data")
 local rank = data and data:FindFirstChild("Christmas_Coins")
 local value = rank and math.floor(rank.Value + 0.5) or "N/A"
 
-Rayfield:Notify({
-    Title = "Ranked Points",
-    Content = "Current Ranked Points: " .. tostring(value),
-    Duration = 5,
-    Image = 4483362458,
-})
+if Rayfield then
+    Rayfield:Notify({
+        Title = "Ranked Points",
+        Content = "Current Ranked Points: " .. tostring(value),
+        Duration = 5,
+        Image = 4483362458,
+    })
+end
