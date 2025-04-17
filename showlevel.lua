229ca2d433cf9abfe4d7f5a8142c1a51e452f010
hydzns <hydrogz7@gmail.com>
2025-04-17 16:44:03 +0700

    Create showlevel.lua

---
+++ showlevel.lua
@@ -0,0 +1,11 @@
+local player = game.Players.LocalPlayer
+local data = player:FindFirstChild("Data")
+local level = data and data:FindFirstChild("Level")
+local value = level and level.Value or "N/A"
+
+Rayfield:Notify({
+    Title = "Player Level",
+    Content = "Current Level: " .. tostring(value),
+    Duration = 5,
+    Image = 4483362458,
+})
